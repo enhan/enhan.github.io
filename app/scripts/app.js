4f9eb4c93d03f1e89755f39dbcc728055d9ee2fd
Emmanuel Nhan <nhanmanu@gmail.com>
2014-07-06 22:58:35 +0200

    yeoman generated site

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name enhangithubioApp
+ * @description
+ * # enhangithubioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('enhangithubioApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name enhangithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the enhangithubioApp
+ */
+angular.module('enhangithubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
